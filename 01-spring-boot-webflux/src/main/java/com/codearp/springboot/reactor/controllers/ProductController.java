9017daf4fde145b2cca71616db9a60d7f45a248d
Andrés Ruiz Peñuela <andresruizpenuela@gmail.com>
2026-01-01 21:45:15 +0100

    [PRJ0001] Programacion reactiva: - modificar el resutlado de un dao antes de emitirlo a la vista   - nombres de productos a mayusucla - añadir un observable para mostrar información de logging

--- 01-spring-boot-webflux/src/main/java/com/codearp/springboot/reactor/controllers/ProductController.java
+++ 01-spring-boot-webflux/src/main/java/com/codearp/springboot/reactor/controllers/ProductController.java
@@ -1,13 +1,17 @@
 package com.codearp.springboot.reactor.controllers;
 
 import com.codearp.springboot.reactor.dao.ProductDao;
+import com.codearp.springboot.reactor.models.documents.Product;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import reactor.core.publisher.Flux;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class ProductController {
 
     private final ProductDao productDao;
@@ -16,8 +20,20 @@ public class ProductController {
     @GetMapping("/products")
     public String listProducts(Model model) {
 
+        // Obtener el Flux y transformar nombres a mayúsculas
+        Flux<Product> productsFlux = productDao.findAll();
+        productsFlux = productsFlux
+                .map(p -> {
+                    p.setName(p.getName() != null ? p.getName().toUpperCase() : null);
+                    return p;
+                });
+
+        // Suscribirse para imprimir los nombres de los productos - observer one
+        productsFlux.subscribe(p -> log.info(p.getName()));
+
         model.addAttribute("title", "Product List");
-        model.addAttribute("products", productDao.findAll());
+        // observer two - thymeleaf view
+        model.addAttribute("products", productsFlux);
         return "products/list";
     }
 }
