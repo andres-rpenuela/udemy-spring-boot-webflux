9fe9201a6f5e678e74bfa4b2e8835c87e0a643fe
Andrés Ruiz Peñuela <andresruizpenuela@gmail.com>
2026-01-02 07:30:55 +0100

    [PRJ0001] Programacion reactiva: - contrapresión, ReactiveDataDriverContextVariable, modificar el número de elementos

--- 01-spring-boot-webflux/src/main/java/com/codearp/springboot/reactor/controllers/ProductController.java
+++ 01-spring-boot-webflux/src/main/java/com/codearp/springboot/reactor/controllers/ProductController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring6.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
 
 @Controller
@@ -34,6 +35,58 @@ public class ProductController {
         model.addAttribute("title", "Product List");
         // observer two - thymeleaf view
         model.addAttribute("products", productsFlux);
+        return "products/list";
+    }
+
+    @GetMapping("/products-data-driver-bloqueante")
+    public String listProductsDataDriverBloqueante(Model model) {
+
+        // Obtener el Flux y transformar nombres a mayúsculas
+        Flux<Product> productsFlux = productDao.findAll()
+                .map(p -> {
+                    p.setName(p.getName() != null ? p.getName().toUpperCase() : null);
+                    return p;
+                })
+                // Simular retardo de 1 segundo por elemento
+                // Para ver el efecto del data driver en la vista
+                // esto hace que los elementos se emitan uno a uno con un retraso
+                // y no todos de golpe, lo que permite ver la carga progresiva en la vista (en caso de usar clientes reactivos) o
+                // o espera para visualizar hasta que todos esten cargados (en caso de usar thymeleaf normal)
+                .delayElements(java.time.Duration.ofSeconds(1));
+
+        // Suscribirse para imprimir los nombres de los productos - observer one
+        productsFlux.subscribe(p -> log.info(p.getName()));
+
+        model.addAttribute("title", "Product List");
+        // observer two - thymeleaf view
+        model.addAttribute("products", productsFlux);
+        return "products/list";
+    }
+
+
+    @GetMapping("/products-data-driver")
+    public String listProductsDataDriver(Model model) {
+
+        // Obtener el Flux y transformar nombres a mayúsculas
+        Flux<Product> productsFlux = productDao.findAll()
+                .map(p -> {
+                    p.setName(p.getName() != null ? p.getName().toUpperCase() : null);
+                    return p;
+                })
+                // Simular retardo de 1 segundo por elemento
+                // Para ver el efecto del data driver en la vista
+                // esto hace que los elementos se emitan uno a uno con un retraso
+                // y no todos de golpe, lo que permite ver la carga progresiva en la vista (en caso de usar clientes reactivos) o
+                // o espera para visualizar hasta que todos esten cargados (en caso de usar thymeleaf normal)
+                .delayElements(java.time.Duration.ofSeconds(1));
+
+        // Suscribirse para imprimir los nombres de los productos - observer one
+        productsFlux.subscribe(p -> log.info(p.getName()));
+
+        model.addAttribute("title", "Product List");
+        // observer two - thymeleaf view
+        // se modifica el buffer de datos para que Thymeleaf procese los elementos a medida que llegan
+        model.addAttribute("products", new ReactiveDataDriverContextVariable(productsFlux, 2));
         return "products/list";
     }
 }
